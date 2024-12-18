b238351690b41cf452467ce2de11cf8ab3c4d649
Tomasz Petrykowski <tomasz.petrykowski.20@gmail.com>
2024-12-18 15:33:20 +0100

    Code update and add tilesets

--- game/entities/character.py
+++ game/entities/character.py
@@ -1,14 +1,23 @@
+from __future__ import (
+    annotations,
+)  # Jeśli chcemy otypować coś, co jeszcze nie jest ukończone (np klasę w samej definicji klasy)
+import pygame as pg
+from random import randint
+
+
 class Character:
-    def __init__(self, name: str, life_points: int, attack_power: int):
+    def __init__(self, name: str, hp: int, attack_power: int):
         self.name = name
-        self.life_points = life_points
+        self.hp = hp
         self.attack_power = attack_power
 
-    def attack(self, target):
-        target.life_points -= self.attack_power
+    def attack(self, target: Character) -> None:
+        damage: int = randint(self.attack_power - 5, self.attack_power + 5)
+        target.hp -= damage
+        print(f"{self.name} attacks {target.name} for {damage} damage!")
 
-    def describe(self):
+    def describe(self) -> None:
         print(f"I'm {self.name}")
 
-    def is_alive(self):
-        return self.life_points > 0
+    def is_alive(self) -> bool:
+        return self.hp > 0
--- game/entities/hero.py
+++ game/entities/hero.py
@@ -1,16 +1,18 @@
 from .character import Character
+import pygame as pg
+from pygame.surface import SurfaceType  # Customowy typ powierzchni w pygame
+from pygame.typing import RectLike
+from game.settings import *
 
 
 class Hero(Character):
-    def __init__(self, name: str, life_points: int, attack_power: int, class_name: str):
-        super().__init__(name, life_points, attack_power)
-        self.class_name = class_name
+    def __init__(self, name: str, hp: int, attack_power: int):
+        super().__init__(name, hp, attack_power)
 
-    # Nadpisaliśmy metodę z klasy nadrzędnej
-    def describe(self):
-        print(f"I'm {self.name} of class {self.class_name}")
-
-    # nadpisaliśmy metodę attack tylko częściowo
-    def attack(self, target):
-        super().attack(target)
-        print(f"Monster {target.name} has been hit")
+        self.image: SurfaceType = pg.image.load(
+            "./assets/images/knight.png"
+        ).convert_alpha()
+        self.image: SurfaceType = pg.transform.scale_by(self.image, SCALE_FACTOR)
+        self.image: SurfaceType = pg.transform.flip(self.image, True, False)
+        self.rect: RectLike = self.image.get_rect()
+        self.rect.center = (200, HEIGHT - 300)
